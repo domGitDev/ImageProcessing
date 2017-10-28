09231285a7849b0c9e1fc34a9b68866fb2e3225d
domGitDev <silvadom2014@gmail.com>
2017-10-28 19:07:16 +0100

    Create Enhancement.h
    class header file

---
+++ Enhancement.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <memory>
+#include "opencv2\opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+using ContourPair = std::pair<std::shared_ptr<Mat>, std::vector<Rect>>;
+
+class Enhancement
+{
+public:
+	// IO
+	static void InitFromFile(string image_path);
+	static void InitFromMat(Mat& image_data);
+	static std::shared_ptr<Enhancement> GetInstancePtr();
+	static std::shared_ptr<Mat> GetGrayImage(Mat& img, bool show);
+	static string BuildFullPathname(string directory, string filename, string ext);
+	static auto ImagesPathFromDirectory(string directory, vector<string> types);
+
+	std::shared_ptr<Mat> GetImage();
+
+	// PRE-PROCESSING
+	Mat EqualizeHistogram(bool show);
+	Mat EqualizeClahe(bool show);
+  std::shared_ptr<Mat> GaussianFilter(Mat& img, bool show);
+	std::shared_ptr<Mat> GammaCorrection(Mat& img, float gamma, bool show);
+	std::pair<std::shared_ptr<Mat>, bool> ComputeHistogram(Mat& img, bool show);
+  
+	std::shared_ptr<Mat> MatchPattern(std::shared_ptr<Mat> baseImg, Mat& pattern, int method);
+	ContourPair ExtractContorsBox(bool show);
+	
+
+private:
+	Enhancement(string image_path);
+	Enhancement(Mat& image_data);
+
+	static std::shared_ptr<Enhancement> InstancePtr;
+	std::shared_ptr<Mat> image_ptr;
+};
+
+
+
+
